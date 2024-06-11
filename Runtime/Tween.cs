a464ae967717f908b6d9ef993e0d76c6f06f4c75
superjellie <dmitry.kiselev01@gmail.com>
2024-06-11 19:45:17 +0300

    added curve tweener

---
+++ Runtime/Tween.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Jenga {
+    public static class Tween {
+        public static Coroutine AlongCurve(
+            MonoBehaviour master, System.Action<float> tweener,
+            AnimationCurve curve, float duration
+        ) {
+            IEnumerator Play() {
+                var start = Time.time;
+                var end = start + duration;
+
+                for (float t = start; t < end; t = Time.time) {
+                    float g = curve.Evaluate((t - start) / duration);
+                    tweener(g);
+                    yield return new WaitForFixedUpdate();
+                }
+
+                tweener(1f);
+            }
+
+            return master.StartCoroutine(Play());
+        }
+    }
+}
